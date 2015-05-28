db5eb11ac4eba198caac2a0ffd9ad04a4773572f
falsecard <falsecard@users.noreply.github.com>
2015-05-28 16:22:48 +0100

    Ripley's K function on Japanese Pines data

--- ripleysK.R
+++ ripleysK.R
@@ -8,6 +8,12 @@ data(japanesepines, redwoodfull, cells)
 ### Second order method - Ripley's K-function ###
 #################################################
 par(pin=c(2.5, 2.5))
-plot(envelope(redwoodfull, Kest), main="Redwood Pines", legend=F)
-plot(envelope(japanesepines, Kest), main="Japanese Pines", legend=F)
-plot(envelope(cells, Kest), main="Cells Pines", legend=F)
+plot(envelope(redwoodfull, Kest), main="Redwood", legend=F)
+plot(envelope(japanesepines, Kest), main="Japanese", legend=F)
+plot(envelope(cells, Kest), main="Cells", legend=F)
+
+# Square root transformation for ease of interpretation
+par(pin=c(2.5, 2.5))
+plot(envelope(redwoodfull, Kest, transform=expression(sqrt(.)), global=TRUE), main="Redwood", legend=F)
+plot(envelope(japanesepines, Kest, transform=expression(sqrt(.)), global=TRUE), main="Japanese", legend=F)
+plot(envelope(cells, Kest, transform=expression(sqrt(.)), global=TRUE), main="Cells", legend=F)
