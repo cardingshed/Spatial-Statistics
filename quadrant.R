09cd4feb7b9bb90a6894b3604b7d32a25c9a2875
falsecard <falsecard@users.noreply.github.com>
2015-05-28 16:22:23 +0100

    Simple Quadrant Method

--- quadrant.R
+++ quadrant.R
@@ -8,7 +8,7 @@ data(japanesepines, redwoodfull, cells)
 ### Quadrant method ###
 #######################
 par(pin=c(4.5,4.5))
-plot(redwoodfull, pch=3, pin=8, main="Redwood Trees")
+plot(redwoodfull, pch=3, pin=8, main="Redwood")
 plot(quadratcount(redwoodfull, nx=3, ny=3), add=T, col="red")
 # Test by chi-sq goodness of fit - result highly dependent on pattern of division
 quadrat.test(redwoodfull, nx=3, ny=3)
